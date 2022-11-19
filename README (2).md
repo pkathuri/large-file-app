ffa5ef56465573fc089ddab65ce74a6510ddc4d3
pkathuri <pkathuri19@gmail.com>
2022-11-19 12:14:41 +0300

    commit for production

--- README (2).md
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:10b377903c4d84e3026f3ec42f175280c4ced96dfee4c163ad2264bc6500caf8
-size 1672
--- models/train_classifier.py
+++ models/train_classifier.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:846ab7d9c770688a5dd0dea8928dcdb8e55ed5806974de885c64f0290fe13f07
-size 5602
+oid sha256:e19a6fcf53fff970dd93cb12b8a8dcbb125f53544d8c8c2fd09edb2aab337d87
+size 5605
